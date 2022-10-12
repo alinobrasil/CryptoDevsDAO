bc9fa4bf3f20848aab2c3727b17b16f1b1ac3b7f
Ali Kim <alikim@gmail.com>
2022-10-12 11:36:03 -0400

    initial commit

---
+++ frontend/constants/index.js
@@ -0,0 +1,5 @@
+export const CRYPTODEVS_DAO_CONTRACT_ADDRESS = "0x88cb1C003b76d88D7266659f99598e285E5B8F62";
+export const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x98877331E8D53dD0Cd75bd75f6DdEdB0388CbD15";
+
+export const CRYPTODEVS_DAO_ABI = require("./CryptoDevsDAO.json").abi;
+export const CRYPTODEVS_NFT_ABI = require("./CryptoDevs.json").abi;
---
+++ hardhat/constants/index.js
@@ -0,0 +1,9 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0x98877331E8D53dD0Cd75bd75f6DdEdB0388CbD15";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
+
+
+// FakeNFTMarketplace deployed to:  0x47990B2A027308bC937113a13A6Ed4856BC3311E
+// CryptoDevsDAO deployed to:  0x88cb1C003b76d88D7266659f99598e285E5B8F62
